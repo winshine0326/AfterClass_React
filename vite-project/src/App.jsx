d4b34bb79b701323f6fed874e8ac473da047eddd
winshine0326 <shane10340@gmail.com>
2024-12-11 18:01:12 +0900

    docs : 댓글 데이터 파일 분리

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,26 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Comment from './Comment.jsx';
+import newComments from './newComments.js';
 // import './App.css'
 
-const comments = [
-  {
-    name : "류승찬",
-    comment : "하이요",
-  },
-  {
-    name : "아니",
-    comment : "진짜로",
-  },
-  {
-    name : "밥",
-    comment : "배고프다",
-  },
-  {
-    name : "집",
-    comment : "가고싶다",
-  },
-]
+const comments = newComments;
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ vite-project/src/newComments.js
@@ -0,0 +1,52 @@
+const comments = [
+    {
+      name : "류승찬",
+      comment : "하이요",
+    },
+    {
+      name : "아니",
+      comment : "진짜로",
+    },
+    {
+      name : "밥",
+      comment : "배고프다",
+    },
+    {
+      name : "집",
+      comment : "가고싶다",
+    },
+    {
+        name : "류승찬2",
+        comment : "하이요",
+      },
+      {
+        name : "아니2",
+        comment : "진짜로",
+      },
+      {
+        name : "밥2",
+        comment : "배고프다",
+      },
+      {
+        name : "집2",
+        comment : "가고싶다",
+      },
+      {
+        name : "류승찬3",
+        comment : "하이요",
+      },
+      {
+        name : "아니3",
+        comment : "진짜로",
+      },
+      {
+        name : "밥3",
+        comment : "배고프다",
+      },
+      {
+        name : "집3",
+        comment : "가고싶다",
+      },
+  ]
+
+  export default comments;
